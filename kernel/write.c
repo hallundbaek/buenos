a82ff1e8c2df4655411e4ea32bfb76a3f13bb072
nikolajhoyer0 <nikolaj@mba.lan>
2014-02-17 22:43:06 +0100

    Added comments on read and write, both in .c files and in report

--- kernel/write.c
+++ kernel/write.c
@@ -2,15 +2,23 @@
 #include "drivers/gcd.h"
 #include "drivers/device.h"
 
-int write_kernel(int fhandle,const void *buffer, int length){
+int write_kernel(int fhandle, const void *buffer, int length){
   int len;
+  device_t *dev;
+  gcd_t *gcd;
+
+  /* Checks that the file handle is the write code (1)
+   * otherwise return 0 since we wont write any characters.
+   * fhandle itself is not needed at this point of implementation.
+   */
   if (fhandle != 1) {
     return 0;
   }
-  device_t *dev;
-  gcd_t *gcd;
-  dev = device_get(YAMS_TYPECODE_TTY, 0 );
+
+  // Get the default TTY and generic device driver
+  dev = device_get(YAMS_TYPECODE_TTY, 0);
   gcd = (gcd_t *)dev->generic_device;
+  // Write to the buffer
   len = gcd->write(gcd, buffer, length);
   return len;
 }
